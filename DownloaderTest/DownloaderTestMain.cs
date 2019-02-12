1ce664a109315a3940692fe6ef79d8d0b70d213e
LSKeegan <45316281+LSKeegan@users.noreply.github.com>
2019-02-11 21:59:33 -0500

    Added yield return

--- DownloaderTest/DownloaderTestMain.cs
+++ DownloaderTest/DownloaderTestMain.cs
@@ -9,50 +9,62 @@ namespace DownloaderTestMain
 {
     class DownloaderTest
     {
+
+        public static void OnDownloadCompleted(Uri Uri, byte[] data)
+        {
+            Console.WriteLine("Downloaded: {0}  Here's my data: {1}", Uri, data);
+        }
+
+        
         static void Main(string[] args)
         {
             ResponseGrabber responseHandler = new ResponseGrabber();
+            List<Uri> uriList = new List<Uri>();
+
+            //Arg input for number of iterations through downloader 
+            int n = 0;
+
+
+            //Check for arg input
+            if ((args.Length != 0) && (args[0] != null))
+            {
+                n = Int32.Parse(args[0]);
+            }
 
             //These will be added immediately to our List 
             Uri test1 = new Uri("https://www.reddit.com/r/Saints/comments/a7610k/official_week_15_game_thread_new_orleans_saints/");
             Uri test2 = new Uri("https://docs.microsoft.com/en-us/dotnet/standard/collections/thread-safe/blockingcollection-overview");
             Uri test3 = new Uri("https://twitter.com/");
-
-            //These will be added after our Downloader.exe runs, simulating a 'stream'
             Uri test4 = new Uri("https://www.hulu.com/");
             Uri test5 = new Uri("https://www.reddit.com/r/JUSTNOMIL/comments/aotnk2/slappy_has_finally_been_arrested/");
             Uri test6 = new Uri("https://www.reddit.com/r/nfl/comments/aoutu2/highlight_brady_micd_up_after_faking_a_block_to/");
 
-            ConcurrentQueue<Uri> uriQueue = new ConcurrentQueue<Uri>();
-            uriQueue.Enqueue(test1);
-            uriQueue.Enqueue(test2);
-            uriQueue.Enqueue(test3);
+            uriList.Add(test1);
+            uriList.Add(test2);
+            uriList.Add(test3);
+            uriList.Add(test4);
+            uriList.Add(test5);
+            uriList.Add(test6);
 
-            void OnDownloadCompleted(Uri Uri, byte[] data)
+            void GetResponsesWithYield()
             {
-                Console.WriteLine("Downloaded: {0}  Here's my data: {1}", Uri, data);
+                IEnumerable<Uri> uriEnumerable = GetTestURLs(n);
+                responseHandler.GetMultipleWebResponses(uriEnumerable, OnDownloadCompleted);
             }
 
-            void AddURLsToQueue()
+            //This simulates 'streaming'
+            IEnumerable<Uri> GetTestURLs(int streamCount)
             {
-                uriQueue.Enqueue(test4);
-                uriQueue.Enqueue(test5);
-                uriQueue.Enqueue(test6);
+                for (var i = 0; i < streamCount; i++)
+                {
+                    Uri uri = uriList[i];
+                    yield return uri;
+                }
             }
 
-            //This thread will run downloader.exe
-            Thread downloadResponses = new Thread( () => responseHandler.GetMultipleWebResponses(uriQueue, OnDownloadCompleted));
-            //This thread will be 'streaming' URLs into our List 
-            Thread streamInput = new Thread( () => AddURLsToQueue());
-           
-            //Start Downloader.exe
-            downloadResponses.Start();
-            //Start 'streaming' Urls into our List
-            streamInput.Start();
-            downloadResponses.Join();
-            streamInput.Join();
-
-            
+            Thread downloadResponsesWithYield = new Thread(() => GetResponsesWithYield());
+            downloadResponsesWithYield.Start();
+              
 
             Console.ReadKey();
         }
